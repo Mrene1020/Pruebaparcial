bfc5ca438d937d59f5714b5255f5cab024a537a0
Your Name <you@example.com>
2019-03-30 10:59:03 -0400

    Commit conmodelo  Mvc

---
+++ parcial/MVCparcial/Models/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+
+namespace MVCparcial.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base ("Default Connection")
+    { }
+
+        protected DataContext(DbCompiledModel model) : base(model)
+        {
+        }
+
+        public System.Data.Entity.DbSet<MVCparcial.Models.Product> Products { get; set; }
+    }
+}
